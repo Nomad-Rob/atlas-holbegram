46c733d4c53437058fe86f193d9ad08c4b648720
Nomad-Rob <rfarley89@gmail.com>
2024-06-24 19:19:23 -0500

    Task 13 complete

--- holbegram/lib/screens/pages/search.dart
+++ holbegram/lib/screens/pages/search.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class Search extends StatelessWidget {
   @override
@@ -7,8 +10,54 @@ class Search extends StatelessWidget {
       appBar: AppBar(
         title: Text('Search'),
       ),
-      body: Center(
-        child: Text('Search Page'),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 8.0),
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Search',
+                  border: InputBorder.none,
+                  prefixIcon: Icon(Icons.search),
+                ),
+              ),
+            ),
+            SizedBox(height: 16),
+            Expanded(
+              child: StreamBuilder(
+                stream: FirebaseFirestore.instance.collection('posts').snapshots(),
+                builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(child: CircularProgressIndicator());
+                  }
+                  if (!snapshot.hasData) {
+                    return Center(child: Text('No images found'));
+                  }
+                  return StaggeredGridView.countBuilder(
+                    crossAxisCount: 2,
+                    itemCount: snapshot.data!.docs.length,
+                    itemBuilder: (context, index) {
+                      var post = snapshot.data!.docs[index];
+                      return Image.network(
+                        post['postUrl'],
+                        fit: BoxFit.cover,
+                      );
+                    },
+                    staggeredTileBuilder: (index) => StaggeredTile.fit(1),
+                    mainAxisSpacing: 8.0,
+                    crossAxisSpacing: 8.0,
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
