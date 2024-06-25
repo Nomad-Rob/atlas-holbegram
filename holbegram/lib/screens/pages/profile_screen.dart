7e22999bb9c804f136294b8c08b48ac7e8126bc3
Nomad-Rob <rfarley89@gmail.com>
2024-06-25 16:24:27 -0500

    Task 15 complete

--- holbegram/lib/screens/pages/profile_screen.dart
+++ holbegram/lib/screens/pages/profile_screen.dart
@@ -1,14 +1,130 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:holbegram/models/user.dart';
+import 'package:holbegram/providers/user_provider.dart';
+import 'package:holbegram/utils/posts.dart';
+import 'package:holbegram/screens/login_screen.dart';
 
-class Profile extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<UserProvider>(context).getUser;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Profile'),
+        title: Text(
+          'Holbegram',
+          style: TextStyle(
+            fontFamily: 'Billabong',
+            fontSize: 32,
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: () async {
+              await _auth.signOut();
+              Navigator.of(context).pushReplacement(
+                MaterialPageRoute(
+                  builder: (context) => LoginScreen(
+                    emailController: TextEditingController(),
+                    passwordController: TextEditingController(),
+                  ),
+                ),
+              );
+            },
+          ),
+        ],
       ),
-      body: Center(
-        child: Text('Profile Page'),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: 40,
+                  backgroundImage: NetworkImage(user.photoUrl),
+                ),
+                SizedBox(width: 16),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      user.username,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24,
+                      ),
+                    ),
+                    Text(
+                      user.email,
+                      style: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: StreamBuilder(
+              stream: FirebaseFirestore.instance
+                  .collection('posts')
+                  .where('uid', isEqualTo: user.uid)
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(child: CircularProgressIndicator());
+                }
+                var posts = snapshot.data!.docs;
+                return StaggeredGridView.countBuilder(
+                  crossAxisCount: 2,
+                  itemCount: posts.length,
+                  itemBuilder: (context, index) {
+                    var post = posts[index];
+                    return PostTile(post: post);
+                  },
+                  staggeredTileBuilder: (index) => StaggeredTile.fit(1),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class PostTile extends StatelessWidget {
+  final DocumentSnapshot post;
+
+  const PostTile({Key? key, required this.post}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image.network(post['postUrl']),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(post['caption']),
+          ),
+        ],
       ),
     );
   }
